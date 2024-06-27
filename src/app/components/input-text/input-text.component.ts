9a1549b292154bbfe6a5cf174dc5fac0574b1502
poomipat-k <k.poomipat@gmail.com>
2024-06-27 16:04:04 +0700

    auth field red border when error

--- src/app/components/input-text/input-text.component.ts
+++ src/app/components/input-text/input-text.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, input, output } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-com-input-text',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './input-text.component.html',
   styleUrl: './input-text.component.scss',
 })
@@ -18,6 +19,7 @@ export class InputTextComponent {
   height = input('42px');
   margin = input('0');
   fontSize = input('16px');
+  error = input(false);
 
   keyUp = output<KeyboardEvent>();
 
