c6e1d5e6c6f17238245ce3984c6df7abb76def52
poomipat-k <k.poomipat@gmail.com>
2024-06-28 16:56:38 +0700

    feature/plan use animation height *

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,13 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject, signal } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  inject,
+  signal,
+  viewChild,
+} from '@angular/core';
 import { PlanService } from '../services/plan.service';
 import { ThemeService } from '../services/theme.service';
 import { PlanCard } from '../shared/models/plan-card';
@@ -15,23 +22,24 @@ import { PreviewPlan } from '../shared/models/preview-plan';
   animations: [
     trigger('slide', [
       transition(':enter', [
-        style({ opacity: 0, maxHeight: 0, padding: '0 32px' }),
+        style({ opacity: 0, height: 0, padding: '0 32px' }),
         animate(
           '300ms ease-out',
-          style({ opacity: 1, maxHeight: '300px', padding: '32px' })
+          style({ opacity: 1, height: '*', padding: '32px' })
         ),
       ]),
       transition(':leave', [
-        style({ opacity: 1, maxHeight: '300px', padding: '32px' }),
+        style({ opacity: 1, height: '*', padding: '32px' }),
         animate(
           '300ms ease-out',
-          style({ opacity: 0, maxHeight: 0, padding: '0 32px' })
+          style({ opacity: 0, height: 0, padding: '0 32px' })
         ),
       ]),
     ]),
   ],
 })
 export class HomeComponent implements OnInit {
+  test = viewChild<ElementRef>('test');
   protected themeService: ThemeService = inject(ThemeService);
   private readonly planService: PlanService = inject(PlanService);
 
@@ -55,6 +63,7 @@ export class HomeComponent implements OnInit {
   }
 
   onCardClick(index: number) {
+    console.log('===test', this.test()?.nativeElement.clientHeight);
     this.plans.update((oldValue) => {
       oldValue[index].open = !oldValue[index].open;
       return oldValue;
