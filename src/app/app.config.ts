1d9fa6e3f1fa7b0f4d7e10c0684c0289ef3cb48c
poomipat-k <k.poomipat@gmail.com>
2024-06-28 15:56:55 +0700

    auth working animation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { authInterceptor } from './shared/interceptors/auth.interceptor';
 
@@ -10,5 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withViewTransitions()),
     provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimations(),
   ],
 };
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,63 @@
+import { animate, style, transition, trigger } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { PlanService } from '../services/plan.service';
 import { ThemeService } from '../services/theme.service';
+import { PlanCard } from '../shared/models/plan-card';
 import { PreviewPlan } from '../shared/models/preview-plan';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
+  animations: [
+    trigger('slide', [
+      transition(':enter', [
+        style({ opacity: 0, maxHeight: 0, padding: '0 32px' }),
+        animate(
+          '300ms linear',
+          style({ opacity: 1, maxHeight: '150px', padding: '32px' })
+        ),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1, maxHeight: '150px', padding: '32px' }),
+        animate(
+          '300ms linear',
+          style({ opacity: 0, maxHeight: 0, padding: '0 32px' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class HomeComponent implements OnInit {
   protected themeService: ThemeService = inject(ThemeService);
   private readonly planService: PlanService = inject(PlanService);
 
-  protected plans = signal<PreviewPlan[]>([]);
+  protected plans = signal<PlanCard[]>([]);
 
   ngOnInit(): void {
     this.themeService.changeTheme('gold');
 
     this.planService.getAllPreviewPlan().subscribe((res: PreviewPlan[]) => {
-      console.log('==res', res);
       if (res && res.length > 0) {
-        this.plans.set(res);
+        this.plans.set(
+          res.map((plan) => {
+            const item = new PlanCard();
+            item.data = plan;
+            item.open = false;
+            return item;
+          })
+        );
       }
+    });
+  }
+
+  onCardClick(index: number) {
+    this.plans.update((oldValue) => {
+      oldValue[index].open = !oldValue[index].open;
+      return oldValue;
     });
   }
 }
