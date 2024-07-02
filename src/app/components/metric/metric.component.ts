fcb5ce3104c907b2b34c3cd991e5350fb4ce5669
poomipat-k <k.poomipat@gmail.com>
2024-07-03 00:21:19 +0700

    feature/plan init metric component

--- src/app/components/metric/metric.component.ts
+++ src/app/components/metric/metric.component.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-com-metric',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './metric.component.html',
   styleUrl: './metric.component.scss',
 })
 export class MetricComponent {
-  protected gridItems = signal([...Array(100).keys()]);
+  xLabel = input<string>('Willingness');
+  yLabel = input<string>('Capacity');
+
+  protected gridItems = signal([
+    'g g g h h h i i i i',
+    'g g g h h h i i i i',
+    'g g g h h h i i i i',
+    'g g g h h h i i i i',
+    'd d d e e e f f f f',
+    'd d d e e e f f f f',
+    'd d d e e e f f f f',
+    'a a a b b b c c c c',
+    'a a a b b b c c c c',
+    'a a a b b b c c c c',
+  ]);
+
+  getItemArea(item: string) {
+    return item;
+  }
 }
