1a90095591a71a9f4ed96e566833e3d134e567a7
poomipat-k <k.poomipat@gmail.com>
2024-07-02 21:49:04 +0700

    feature/plan edit button

--- src/app/plan-details/plan-details.component.ts
+++ src/app/plan-details/plan-details.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, inject, input } from '@angular/core';
+import { Component, OnInit, inject, input, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { PlanService } from '../services/plan.service';
 import { ThemeService } from '../services/theme.service';
+import { PlanDetails } from '../shared/models/plan-details';
 
 @Component({
   selector: 'app-plan-details',
@@ -13,6 +14,8 @@ import { ThemeService } from '../services/theme.service';
 export class PlanDetailsComponent implements OnInit {
   protected planName = input<string>();
 
+  protected planDetails = signal<PlanDetails>(new PlanDetails());
+
   protected readonly themeService: ThemeService = inject(ThemeService);
   private readonly planService: PlanService = inject(PlanService);
   private readonly router: Router = inject(Router);
@@ -21,9 +24,14 @@ export class PlanDetailsComponent implements OnInit {
     this.themeService.changeTheme('silver');
 
     console.log('==planName', this.planName());
-    this.planService.getPlanDetails(this.planName() || '').subscribe((res) => {
-      console.log('==plan result', res);
-    });
+    this.planService
+      .getPlanDetails(this.planName() || '')
+      .subscribe((planDetails) => {
+        console.log('==plan planDetails', planDetails);
+        if (planDetails) {
+          this.planDetails.set(planDetails);
+        }
+      });
   }
 
   onBackToHomePage() {
