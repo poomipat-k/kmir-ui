18b6b4898f46c0e58e4ae834ddda47b0b8bd7138
poomipat-k <k.poomipat@gmail.com>
2024-07-05 17:25:14 +0700

    feature/plan remove editor's quick bar plugint and block drag and drop

--- src/app/components/custom-editor/custom-editor.component.ts
+++ src/app/components/custom-editor/custom-editor.component.ts
@@ -16,10 +16,10 @@ export class CustomEditorComponent implements OnInit {
   control = input(new FormControl());
   protected editorInit = signal<EditorComponent['init']>({});
   protected editorPlugins = signal(
-    'preview autolink autosave save code visualblocks visualchars fullscreen image link media codesample table charmap nonbreaking anchor lists advlist wordcount help charmap quickbars emoticons'
+    'preview autolink autosave save code visualblocks visualchars fullscreen link media codesample table charmap nonbreaking anchor lists advlist wordcount help charmap emoticons'
   );
   protected editorToolbar = signal(
-    'undo redo | blocks fontsize | bold italic underline strikethrough | align numlist bullist | link image | table media | lineheight outdent indent| forecolor backcolor removeformat | charmap emoticons | code fullscreen preview | save print | anchor codesample'
+    'undo redo | blocks fontsize | bold italic underline strikethrough | align numlist bullist | link | table media | lineheight outdent indent| forecolor backcolor removeformat | charmap emoticons | code fullscreen preview | save print | anchor codesample'
   );
 
   ngOnInit(): void {
@@ -30,13 +30,12 @@ export class CustomEditorComponent implements OnInit {
     this.editorInit.set({
       base_url: '/tinymce',
       suffix: '.min',
-      font_size_formats: '10px 12px 14px 16px 18px 20px 24px 28px 32px 48px',
+      font_size_formats:
+        '10px 12px 13px 14px 16px 18px 20px 24px 28px 32px 48px',
       line_height_formats:
-        '10px 16px 18px 20px 24px 27px 30px 33px 36px 40px 48px',
-      images_file_types: 'jpeg,jpg,jpe,jfi,jif,jfif,png,gif,bmp,webp,svg',
-      images_reuse_filename: true,
-      image_advtab: true,
+        '10px 16px 18px 20px 24px 27px 30px 33px 36px 39px 44px 48px 52px',
       block_unsupported_drop: true,
+      paste_block_drop: true,
     });
   }
 }
