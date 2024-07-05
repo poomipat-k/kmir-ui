9563727c6195513c4cbb5aedb2f9e6bd3eedf234
poomipat-k <k.poomipat@gmail.com>
2024-07-05 16:50:03 +0700

    feature/plan header section

---
+++ src/app/components/save-button/save-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-com-save-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './save-button.component.html',
+  styleUrl: './save-button.component.scss',
+})
+export class SaveButtonComponent {}
--- src/app/plan-edit/plan-edit.component.ts
+++ src/app/plan-edit/plan-edit.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, OnInit, inject, input, signal } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IconTooltipComponent } from '../components/icon-tooltip/icon-tooltip.component';
+import { SaveButtonComponent } from '../components/save-button/save-button.component';
+import { PlanService } from '../services/plan.service';
+import { ThemeService } from '../services/theme.service';
+import { PlanDetails } from '../shared/models/plan-details';
 
 @Component({
   selector: 'app-plan-edit',
   standalone: true,
-  imports: [],
+  imports: [IconTooltipComponent, SaveButtonComponent, ReactiveFormsModule],
   templateUrl: './plan-edit.component.html',
-  styleUrl: './plan-edit.component.scss'
+  styleUrl: './plan-edit.component.scss',
 })
-export class PlanEditComponent {
+export class PlanEditComponent implements OnInit {
+  // url params
+  protected planName = input<string>();
 
+  protected form = signal<FormGroup>(new FormGroup({}));
+  protected planDetails = signal<PlanDetails>(new PlanDetails());
+  protected scrollerOffset = signal<[number, number]>([0, 40]); // [x, y]
+
+  protected readonly themeService: ThemeService = inject(ThemeService);
+  private readonly planService: PlanService = inject(PlanService);
+  private readonly router: Router = inject(Router);
+  private readonly scroller: ViewportScroller = inject(ViewportScroller);
+
+  ngOnInit(): void {
+    this.themeService.changeTheme('silver');
+    this.scroller.setOffset(this.scrollerOffset());
+    this.planService
+      .getPlanDetails(this.planName() || '')
+      .subscribe((planDetails) => {
+        console.log('==edit plan planDetails', planDetails);
+        if (planDetails) {
+          this.planDetails.set(planDetails);
+        }
+      });
+  }
+
+  onBackToPlanDetailsPage() {
+    this.router.navigate([`/plan/${this.planName()}`]);
+  }
+
+  onReadinessSaveClick() {
+    console.log('==onReadinessSaveClick');
+  }
 }
